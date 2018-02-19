e0cc764e07cba3613f79f082e39fea4988e7bebe
Dzmitry Zakhar <31858665+SpiritForest@users.noreply.github.com>
2018-02-19 23:16:01 +0300

    triangles
    triangles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 /**
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
- */
+*/
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+// your implementation
+    var a, b, c;
+    var counter = 0;
+    for (var i = 0; i < preferences.length; i++){
+        a = preferences[i];
+        b = preferences[a-1];
+        c = preferences[b-1];
+        if (c == i+1) {
+            counter++;}
+    }
+    return Math.floor(counter/3);
 };
